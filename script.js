d8ca6684c89643ece31522211eb833f787da2f2e
Tim <timothy.yi7@gmail.com>
2017-06-06 19:54:03 -0700

    Memory Match version0.5 - Tim Yi

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(function()){
+
+    first_card_clicked = null;
+    second_card_clicked = null;
+    total_possible_matches = 2;
+    match_counter = 0;
+
+    function card_clicked() {
+        if (first_card_clicked = null){
+            first_card_clicked ==
+        } else if (
+            second_card_clicked ==
+
+            };
+}
+
