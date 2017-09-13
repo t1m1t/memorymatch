ecea1fe8641661a49eb5070bc597101991b26286
Tim <timothy.yi7@gmail.com>
2017-09-13 14:27:59 -0700

    fixed randomize bug

--- script.js
+++ script.js
@@ -72,7 +72,7 @@ function resetCards(){
     for(var i = 0; i < cards.length; i++){
         var target = Math.floor(Math.random() * cards.length - 1) + 1;
         var target2 = Math.floor(Math.random() * cards.length - 1) +1;
-        cards.eq(target).before(cards.eq(target2));
+        cards[target].before(cards[target2]);
     }
 }
 
